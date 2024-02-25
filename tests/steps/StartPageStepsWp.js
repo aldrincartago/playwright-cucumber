230204ebe0a35d69105de8305e869fd550a8f16f
jose-luis-nunez <josenunez@outlook.de>
2024-02-25 19:28:55 +0100

    update

--- tests/steps/StartPageStepsWp.js
+++ tests/steps/StartPageStepsWp.js
@@ -7,7 +7,6 @@ const baseUrl = 'https://www.check24.de/'
 Given('I open Check24 page wp', async function () {
     await this.page.goto(baseUrl, { waitUntil: 'networkidle' });
     await this.page.locator(acceptCookiesButton).click();
-
 });
 
 When('I search for {string} wp', async function (product) {
