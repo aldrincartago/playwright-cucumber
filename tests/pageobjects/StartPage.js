947000f7a7c4daba7303648fbf23324f7dacdb96
jose-luis-nunez <josenunez@outlook.de>
2024-02-25 08:28:47 +0100

    clean up

--- tests/pageobjects/StartPage.js
+++ tests/pageobjects/StartPage.js
@@ -1,12 +1,13 @@
 class StartPage {
     constructor(page) {
         this.page = page;
+        this.baseUrl = 'https://www.check24.de/'
         this.acceptCookiesButton = '.c24-cookie-consent-notice-buttons .c24-cookie-consent-button:nth-child(2)';
         this.searchField = 'input[name=\'q\']';
     }
 
     async open() {
-        await this.page.goto("https://www.check24.de/", { waitUntil: 'networkidle' });
+        await this.page.goto(this.baseUrl, { waitUntil: 'networkidle' });
         await this.page.locator(this.acceptCookiesButton).click();
     }
 
