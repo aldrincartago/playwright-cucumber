69d521281c371acabce84cc1d99779e5b6ae8577
jose-luis-nunez <josenunez@outlook.de>
2024-02-25 19:08:59 +0100

    set timeout in cucumber.js

--- cucumber.js
+++ cucumber.js
@@ -9,6 +9,7 @@ let options = [
   '--require tests/support/GlobalHooks.js', //to use the helper class
   '--format summary',
   '--format json:./test-results/reports/cucumber.json',
+  '--timeout 10000', // increase timeout for steps
 ].join(' ');
 
 let runner = [
